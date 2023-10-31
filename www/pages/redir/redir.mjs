d3b4e5f89bbd2aa599091dc6a9872d0c419b7522
Anders Kristiansen <palantus@gmail.com>
2023-10-31 16:49:07 +0100

    Use new api for stylesheets in components

--- www/pages/redir/redir.mjs
+++ www/pages/redir/redir.mjs
@@ -8,13 +8,11 @@ import "../../components/field-edit-inline.mjs"
 import "../../pages/redir/redirlink.mjs"
 import {on, off} from "../../system/events.mjs"
 import { showDialog, confirmDialog } from "../../components/dialog.mjs";
-import { getApiConfig } from "../../system/core.mjs"
+import { getApiConfig, stylesheets } from "../../system/core.mjs"
 
 
 const template = document.createElement('template');
 template.innerHTML = `
-  <link rel='stylesheet' href='/css/global.css'>
-  <link rel='stylesheet' href='/css/searchresults.css'>
   <style>
 
     #container{
@@ -78,7 +76,8 @@ class Element extends HTMLElement {
   constructor() {
     super();
 
-    this.attachShadow({ mode: 'open' });
+    this.attachShadow({ mode: 'open' })
+        .adoptedStyleSheets = [stylesheets.global, stylesheets.searchresults];
     this.shadowRoot.appendChild(template.content.cloneNode(true));
     this.refreshData = this.refreshData.bind(this);
     this.newItem = this.newItem.bind(this)
--- www/pages/redir/redirlink.mjs
+++ www/pages/redir/redirlink.mjs
@@ -5,13 +5,11 @@ import "../../components/field-ref.mjs"
 import "../../components/field-edit-inline.mjs"
 import "../../components/field-list.mjs"
 import {on, off} from "../../system/events.mjs"
-import { getApiConfig, state } from "../../system/core.mjs"
+import { getApiConfig, state, stylesheets } from "../../system/core.mjs"
 
 
 const template = document.createElement('template');
 template.innerHTML = `
-  <link rel='stylesheet' href='/css/global.css'>
-  <link rel='stylesheet' href='/css/searchresults.css'>
   <style>
     field-list{
       width: 500px;
@@ -60,7 +58,8 @@ class Element extends HTMLElement {
   constructor() {
     super();
 
-    this.attachShadow({ mode: 'open' });
+    this.attachShadow({ mode: 'open' })
+        .adoptedStyleSheets = [stylesheets.global, stylesheets.searchresults];
     this.shadowRoot.appendChild(template.content.cloneNode(true));
     this.refreshData = this.refreshData.bind(this);
     this.deleteLink = this.deleteLink.bind(this)
